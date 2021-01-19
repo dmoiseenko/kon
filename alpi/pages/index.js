a6ddacdd884a3612c628a2d73efd878c0e454b82
Denis Moiseenko <dmoiseenko@mykolab.com>
2021-01-19 13:34:20 +0300

    Fix something

--- alpi/pages/index.js
+++ alpi/pages/index.js
@@ -7,15 +7,29 @@ import PostList, {
   allPostsQueryVars,
 } from '../components/PostList'
 import { initializeApollo, addApolloState } from '../lib/apolloClient'
+import { useUser } from '@auth0/nextjs-auth0'
 
-const IndexPage = () => (
-  <App>
-    <Header />
-    <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
-    <Submit />
-    <PostList />
-  </App>
-)
+const IndexPage = () => {
+  const { user, error, isLoading } = useUser()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+        <App>
+          <Header />
+          <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
+          <Submit />
+          <PostList />
+        </App>
+      </div>
+    );
+  }
+  return <a href="/api/auth/login">Login</a>
+}
 
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
