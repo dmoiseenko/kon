30b6dcf89d9f54a4b78b1f90ba402ccf814b25e6
Denis Moiseenko <dmoiseenko@mykolab.com>
2021-01-20 12:22:44 +0300

    Fix something

--- alpi/pages/api/auth/[...auth0].js
+++ alpi/pages/api/auth/[...auth0].js
@@ -1,15 +1,17 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0"
 
 export default handleAuth({
   async login(req, res) {
     try {
       await handleLogin(req, res, {
-        response_type: 'code',
-        audience: 'https://dev.dmoiseenko/v1/graphql',
-        scope: 'read:all'
-      });
+        authorizationParams: {
+          audience: "https://dev.dmoiseenko/v1/graphql",
+          scope: "openid profile email read:all",
+          response_type: "code",
+        },
+      })
     } catch (error) {
       res.status(error.status || 400).end(error.message)
     }
-  }
+  },
 })
