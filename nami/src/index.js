c52ffa9b8ca7a8a1b134fe2e0272dfa63e2fe35b
Denis Moiseenko <dmoiseenko@mykolab.com>
2021-02-05 18:31:59 +0300

    Fix something

--- nami/src/index.js
+++ nami/src/index.js
@@ -1,24 +1,44 @@
+const { v4: uuidv4 } = require("uuid")
 const fastify = require("fastify")({
   logger: true,
 })
 const { Client } = require("pg")
+fs = require("fs")
+const util = require("util")
+const readFile = util.promisify(fs.readFile)
+
+let connection_string = ""
+let volume_root = ""
+if (process.env["TELEPRESENCE_ROOT"]) {
+  volume_root = process.env["TELEPRESENCE_ROOT"]
+}
 
 fastify.post("/add_todo", async (request, reply) => {
   const client = new Client({
-    connectionString:
-      "postgres://zoru:srE7uSxFwjbOwv4zK4iaFgRTbwqv1Mwz6wZqh24wRLQ66@zoru:5432/kon",
+    connectionString: connection_string,
   })
+
+  const id = uuidv4()
+  const text = request.body.input.text
 
   await client.connect()
 
-  const res = await client.query("SELECT NOW();")
+  const res = await client.query(
+    "INSERT INTO zoru.todo VALUEs($1, $2) RETURNING *;",
+    [id, text]
+  )
 
-  return { id: "world" }
+  return { id: res.rows[0].id }
 })
 
 const start = async () => {
   try {
-    await fastify.listen(3000, '0.0.0.0')
+    await fastify.listen(3000, "0.0.0.0")
+
+    connection_string = await readFile(
+      `${volume_root}/vault/secrets/database-config`,
+      "utf8"
+    )
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
