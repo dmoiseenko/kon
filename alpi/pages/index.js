5643b11498f02c951345fc8686f86c1cb2534f44
Denis Moiseenko <dmoiseenko@mykolab.com>
2021-02-08 16:05:29 +0300

    Fix something

--- alpi/pages/index.js
+++ alpi/pages/index.js
@@ -1,30 +1,25 @@
 import App from "../components/App"
-import InfoBox from "../components/InfoBox"
 import Header from "../components/Header"
 import Submit from "../components/Submit"
 import PostList from "../components/PostList"
 import { useUser } from "@auth0/nextjs-auth0"
 
 const IndexPage = () => {
-  const { user, error, isLoading } = useUser()
+  const { user } = useUser()
 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>{error.message}</div>
-
-  if (user) {
-    return (
-      <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-        <App>
-          <Header />
-          <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
-          <Submit />
-          <PostList />
-        </App>
-      </div>
-    )
-  }
-  return <a href="/api/auth/login">Login</a>
+  return (
+    <div>
+      <App>
+        <Header />
+        {user && (
+          <div>
+            <Submit />
+            <PostList />
+          </div>
+        )}
+      </App>
+    </div>
+  )
 }
 
 export default IndexPage
