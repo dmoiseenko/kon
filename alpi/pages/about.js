8721f45dc9f677f2e2079a0d73bbe54784f14020
Denis Moiseenko <dmoiseenko@mykolab.com>
2021-02-10 11:28:31 +0300

    Fix something

--- alpi/pages/about.js
+++ alpi/pages/about.js
@@ -14,26 +14,6 @@ const AboutPage = () => (
         of your data, which is kept up to date as further queries and mutations
         are run, fetching more results from the server.
       </p>
-      <p>
-        In this simple example, we integrate Apollo seamlessly with{" "}
-        <a href="https://github.com/vercel/next.js">Next</a> by calling{" "}
-        <a href="https://nextjs.org/docs/basic-features/data-fetching#getstaticprops-static-generation">
-          getStaticProps
-        </a>{" "}
-        at our Page component. This approach lets us opt out of getInitialProps
-        and let us use all the niceties provided by{" "}
-        <a href="https://github.com/vercel/next.js">Next</a>.
-      </p>
-      <p>
-        On initial page load, while on the server and inside{" "}
-        <a href="https://nextjs.org/docs/basic-features/data-fetching#getstaticprops-static-generation">
-          getStaticProps
-        </a>
-        , we fetch the query used to get the list of posts. At the point in
-        which the query promise resolves, our Apollo Client store is completely
-        initialized. Then we serve the initial HTML with the fetched data and
-        hydrate Apollo in the browser.
-      </p>
     </article>
   </App>
 )
